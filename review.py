5e0b0e089523f440e3f11ae9317fa079f0b34888
troyyxk <xingkunyin@hotmail.com>
2020-07-09 14:20:17 -0400

    Add new workds.

--- review.py
+++ review.py
@@ -1,27 +1,46 @@
 from xlrd import open_workbook
+from random import shuffle
 
 def main(file_loc):
     r_words = []
 
     wb = open_workbook(file_loc)
-    sheet = wb.sheets()[1]
+    sheet = wb.sheets()[0]
+
     number_of_rows = sheet.nrows
-    for i in range(number_of_rows):
+    indexes = [i for i in range(number_of_rows)]
+    if input("randome? (\"y\" for yes) ") == "y":
+        shuffle(indexes)
+
+    count = 0
+    isExit = False
+    for i in indexes:
+        count += 1
+        is_to_next = False
         word  = sheet.cell(i,0).value
         if word =="":
             continue
         print(word)
         print("Know this one?")
-        res = input(">>> ")
-        if res == "c":
-            print(number_of_rows - i)
+        while not is_to_next:
+            is_to_next = True
             res = input(">>> ")
-        if res == "num":
-            print(len(r_words))
-            res = input(">>> ") 
-        if res != "" and res != "y":
-            r_words.append(word)
-            print("Added to review list")
+            if res == "c":
+                is_to_next = False
+                print(len(r_words))
+            elif res == "r":
+                is_to_next = False
+                print(number_of_rows - count)
+            elif res == "exit":
+                isExit = True
+                break
+            elif res != "" and res != "y":
+                r_words.append(word)
+                is_to_next = True
+                print("Added to review list")
+        
+        if isExit == True:
+            break
         print("----------------------------")
 
     with open('review_list.txt', 'w') as f:
@@ -30,4 +49,4 @@ def main(file_loc):
 
 
 if __name__=="__main__":
-    main("./Vocab_grabbed_on_the_way.xlsx")
+    main("./harsh.xlsx")
